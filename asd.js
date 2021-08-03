7e4d0f7c16a8a92aa3a635ead7f58922500154b2
Nyaka <87859083+dammnyaka@users.noreply.github.com>
2021-08-03 16:52:39 +0300

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,8 @@
+// setInterval( () => {
+
+//     let winHeight = $(window).height();     // высота окна
+//     let divHeight = $('.main').height();  // высота блока
+//     let ratio = winHeight / divHeight;      // отношение между ними
+//     $('.main').css({ zoom: ratio });      // масштабирование блока 
+
+// }, 200);
